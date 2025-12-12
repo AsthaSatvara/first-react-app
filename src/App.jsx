82aa19c8f4a33cfa779312b510bca76ae8a443f4
AsthaSatvara <astha2000.vg@gmail.com>
2025-12-12 13:15:36 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Music from './pages/Music'
+
+function App() {
+
+  return (
+    <>
+    <Music/>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Music.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import Image from '/vite.svg'
+import SrcImage from '../assets/react.svg'
+
+const Music = () => {
+  return (
+    <div>
+        <h1>Music</h1>
+        <img src={SrcImage} alt='vite'/>
+    </div>
+  )
+}
+
+export default Music
